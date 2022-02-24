ce7678ffcb266aaf1fac557145c861d5a111820d
xwchen <1157369690@qq.com>
2022-02-24 10:42:33 +0800

    data基类

--- library/lib_view/src/main/java/com/hjq/shape/view/FastIndexBar.java
+++ library/lib_view/src/main/java/com/hjq/shape/view/FastIndexBar.java
@@ -96,6 +96,22 @@ public class FastIndexBar extends View {
      * 如果是一个View，重写onMeasure时要注意：
      * 如果在使用自定义view时，用了wrap_content。那么在onMeasure中就要调用setMeasuredDimension，
      * 来指定view的宽高。如果使用的fill_parent或者一个具体的dp值。那么直接使用super.onMeasure即可。
+     * <p>
+     * <p>
+     * 如果是一个ViewGroup，重写onMeasure时要注意：
+     * 首先，结合上面两条，来测量自身的宽高。
+     * 然后，需要测量子View的宽高。
+     * 测量子view的方式有：
+     * getChildAt(int index).可以拿到index上的子view。
+     * 通过getChildCount得到子view的数目，再循环遍历出子view。
+     * 接着，subView.measure(int wSpec, int hSpec); //使用子view自身的测量方法
+     * 或者调用viewGroup的测量子view的方法：
+     * //某一个子view，多宽，多高, 内部加上了viewGroup的padding值
+     * measureChild(subView, int wSpec, int hSpec);
+     * //所有子view 都是 多宽，多高, 内部调用了measureChild方法
+     * measureChildren(int wSpec, int hSpec);
+     * //某一个子view，多宽，多高, 内部加上了viewGroup的padding值、margin值和传入的宽高wUsed、hUsed
+     * measureChildWithMargins(subView, intwSpec, int wUsed, int hSpec, int hUsed);
      *
      * @param widthMeasureSpec
      * @param heightMeasureSpec
