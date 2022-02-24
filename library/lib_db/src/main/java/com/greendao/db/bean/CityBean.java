e07a02f5caade27dc956653fba19761b16c38bae
cxw18772827901 <1157369690@qq.com>
2022-02-24 21:33:21 +0800

    混淆

---
+++ library/lib_db/src/main/java/com/greendao/db/bean/CityBean.java
@@ -0,0 +1,32 @@
+package com.greendao.db.bean;
+
+import java.util.List;
+
+/**
+ * PackageName  com.greendao.db.bean
+ * ProjectName  TempleteProject-java
+ * Date         2022/2/24.
+ *
+ * @author chenxiaowu
+ */
+
+public class CityBean {
+
+   /**
+    * name : 北京市
+    * city : [{"name":"北京市","area":["东城区","西城区","海淀区","朝阳区","丰台区","石景山区","门头沟区","通州区","顺义区","房山区","大兴区","昌平区","怀柔区","平谷区","密云区","延庆区"]}]
+    */
+
+   public String name;
+   public List<CityDataBean> city;
+
+   public static class CityDataBean {
+      /**
+       * name : 北京市
+       * area : ["东城区","西城区","海淀区","朝阳区","丰台区","石景山区","门头沟区","通州区","顺义区","房山区","大兴区","昌平区","怀柔区","平谷区","密云区","延庆区"]
+       */
+
+      public String name;
+      public List<String> area;
+   }
+}
