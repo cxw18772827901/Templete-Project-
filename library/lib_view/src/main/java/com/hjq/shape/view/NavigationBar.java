d0c0ae2546a58b9fe231588454761e421e9b71c7
xwchen <1157369690@qq.com>
2022-02-28 10:09:33 +0800

    初始化

--- library/lib_view/src/main/java/com/hjq/shape/view/NavigationBar.java
+++ library/lib_view/src/main/java/com/hjq/shape/view/NavigationBar.java
@@ -61,17 +61,17 @@ public class NavigationBar extends FrameLayout {
         }
         this.tabs = tabs;
         this.listener = listener;
-        int dimension25 = (int) getContext().getResources().getDimension(R.dimen.x25);
+        //int dimension25 = (int) getContext().getResources().getDimension(R.dimen.x25);
         for (int i = 0; i < tabs.size(); i++) {
             String tabStr = tabs.get(i).getStr();
             View view = LayoutInflater.from(getContext()).inflate(R.layout.navigation_bar_item, this, false);
             TextView tv = view.findViewById(R.id.tv);
             tv.setText(tabStr);
             //params
-            FrameLayout.LayoutParams params = (LayoutParams) tv.getLayoutParams();
+            /*FrameLayout.LayoutParams params = (LayoutParams) tv.getLayoutParams();
             params.setMarginStart(i == 0 ? dimension25 : 0);
             params.setMarginEnd(dimension25);
-            tv.setLayoutParams(params);
+            tv.setLayoutParams(params);*/
             //tab
             TabLayout.Tab tab = tabLayout.newTab();
             tab.setCustomView(view);
